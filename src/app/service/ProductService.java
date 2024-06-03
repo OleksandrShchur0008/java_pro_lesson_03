0c0cdbc11c130fb172e3cea41a665e1f58605adf
OleksandrShchur <sasha28shchur@gmail.com>
2024-06-03 18:41:44 +0200

    Product first commit

---
+++ src/app/service/ProductService.java
@@ -0,0 +1,14 @@
+package app.service;
+
+import app.domain.Product;
+
+public interface ProductService {
+
+    Product getById(Long id);
+}
+
+
+
+
+
+
---
+++ src/app/service/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package app.service;
+
+import app.domain.Product;
+import app.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository repository;
+    private String articlePrefix;
+
+    public ProductServiceImpl(ProductRepository repository, @Value("${article.prefix}") String articlePrefix) {
+        this.repository = repository;
+        this.articlePrefix = articlePrefix;
+    }
+
+    @Override
+    public Product getById(Long id) {
+        Product product = repository.getById(id);
+        setArticle(product);
+        return product;
+    }
+
+    private void setArticle(Product product) {
+        String article = String.format("%s-%s-%d", articlePrefix, product.getTitle().charAt(0), product.getId());
+        product.setArticle(article);
+    }
+}
